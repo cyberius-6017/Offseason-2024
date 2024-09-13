adbfbcd38c1fd295762ca7d92ec75e0f4c78c48e
Erick-Pines <erickterana@gmail.com>
2024-09-13 08:19:03 -0600

    Setup Ready
    Setup for new robot/drivetrain.

---
+++ Ventura_2024/src/main/java/frc/robot/subsystems/drivetrain/drivetrain.java
@@ -0,0 +1,422 @@
+package frc.robot.subsystems.drivetrain;
+
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+// import edu.wpi.first.math.kinematics.SwerveDriveWheelPositions;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+
+public class drivetrain extends SubsystemBase{
+
+    private Pigeon2 gyro = new Pigeon2(0);
+
+
+    private swerveModule[] swerveModules;
+    private SwerveModulePosition[] swervePositions;
+
+    private Field2d field;
+    private SwerveDriveOdometry swerveOdometry;
+    private SwerveDrivePoseEstimator swervePoseEstimator;
+    
+
+    public drivetrain(){
+        gyro.getConfigurator().apply(new Pigeon2Configuration());
+        gyro.reset();
+        zeroPigeon();
+        
+
+        field = new Field2d();
+        SmartDashboard.putData("Field: ", field);
+        AutoBuilder.configureHolonomic(this::getPose,
+                                       this::resetPose, 
+                                       this::getRobotRelativeSpeeds, 
+                                       this::driveRobotRelative, 
+                                       Constants.Swerve.holoConfig,
+                                       ()-> {
+                                        var alliance = DriverStation.getAlliance();
+                                        if(alliance.isPresent())
+                                            return alliance.get() == DriverStation.Alliance.Red;
+                                        else
+                                            return false;    
+                                       },
+                                       this);
+        
+        
+        
+        swerveModules =
+            new swerveModule[] {
+                new swerveModule(0, Constants.Swerve.Mod0.constants),
+                new swerveModule(1, Constants.Swerve.Mod1.constants),
+                new swerveModule(2, Constants.Swerve.Mod2.constants),
+                new swerveModule(3, Constants.Swerve.Mod3.constants),
+            };
+
+        swervePositions = 
+            new SwerveModulePosition[]{
+                swerveModules[0].getPosition(),
+                swerveModules[1].getPosition(),
+                swerveModules[2].getPosition(),
+                swerveModules[3].getPosition()
+            };
+            
+        // swerveWheels =
+        //     new SwerveDriveWheelPositions[]{
+        //         new SwerveDriveWheelPositions(swervePositions)
+        //     };
+
+        swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveOdoKinematics, 
+                                                 getOdoYaw(), 
+                                                 swervePositions); 
+        swervePoseEstimator = new SwerveDrivePoseEstimator(Constants.Swerve.swerveOdoKinematics, 
+                                                           getOdoYaw(), 
+                                                           swervePositions, 
+                                                           getPose());
+        field.setRobotPose(getPose());
+        // limef = NetworkTableInstance.getDefault().getTable(Constants.Sensors.limef);
+        // limeb = NetworkTableInstance.getDefault().getTable(Constants.Sensors.limeb);  
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean isFieldDrive, boolean isOpenloop){
+
+        SwerveModuleState[] swerveModuleStates =
+            Constants.Swerve.swerveOdoKinematics.toSwerveModuleStates(isFieldDrive ? ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), 
+                                                                                                                        translation.getY(),
+                                                                                                                        rotation, 
+                                                                                                                        getOdoYaw())
+                                                                                    : new ChassisSpeeds(-translation.getY(),
+                                                                                                         translation.getX(),
+                                                                                                         rotation));
+
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+
+        for(swerveModule module : swerveModules){
+
+            module.setDesiredState(swerveModuleStates[module.moduleID], isOpenloop);
+
+        }
+
+    }
+
+    public void setModuleStates(SwerveModuleState[] desiredStates) {
+
+        SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
+    
+        for (swerveModule module : swerveModules) {
+
+          module.setDesiredState(desiredStates[module.moduleID], false);
+
+        }
+
+      }
+
+    public Rotation2d getYaw() {
+
+        double angle = (Constants.Swerve.invNavX) ? 360 - gyro.getAngle() //NavX is flipped
+                                                  : gyro.getAngle();
+
+        return Rotation2d.fromDegrees(angle); 
+
+    }
+
+    public Rotation2d getOdoYaw() {
+
+        double angle = (!Constants.Swerve.invNavX) ? 360 - gyro.getAngle() //NavX is flipped
+                                                   : gyro.getAngle();
+
+        return Rotation2d.fromDegrees(angle); 
+
+    }
+
+    public void zeroPigeon() {
+
+        System.out.println("Pigean zeroed correctly");
+        gyro.setYaw(0.0);
+
+    }
+
+    public SwerveModuleState[] getStates() {
+        SwerveModuleState[] states = new SwerveModuleState[4];
+
+        for (swerveModule module : swerveModules) {
+
+          states[module.moduleID] = module.getState();
+
+        }
+
+        return states;
+    }
+
+    public SwerveModulePosition[] getPositions() {
+        SwerveModulePosition[] positions = new SwerveModulePosition[4];
+
+        for (swerveModule module : swerveModules) {
+
+          positions[module.moduleID] = module.getPosition();
+
+        }
+
+        return positions;
+    }
+
+    public Pose2d getPose() {
+
+        return swerveOdometry.getPoseMeters();
+        //return swervePoseEstimator.getEstimatedPosition();
+    }
+
+
+    public void resetPose(Pose2d pose) {
+    
+        swerveOdometry.resetPosition(getOdoYaw(), getPositions(), pose);
+    
+    }
+
+    public void driveRobotRelative(ChassisSpeeds speeds){
+        //TODO: Check open loop
+        this.drive(new Translation2d(speeds.vyMetersPerSecond, -speeds.vxMetersPerSecond ),speeds.omegaRadiansPerSecond,false,true);
+    
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds(){
+        
+        return Constants.Swerve.swerveOdoKinematics.toChassisSpeeds(getStates());
+
+    }
+
+    public void goToCoorditanes(double x, double y, Rotation2d rotation){
+        
+        double speedx = (x - swervePoseEstimator.getEstimatedPosition().getX()) * Constants.Swerve.alignKP;
+        double speedy = (y - swervePoseEstimator.getEstimatedPosition().getY()) * Constants.Swerve.alignKP;
+        Rotation2d speedw = Rotation2d.fromDegrees((rotation.getDegrees() - swervePoseEstimator.getEstimatedPosition().getRotation().getDegrees()) * Constants.Swerve.alignKP);
+        drive(new Translation2d(speedx, -speedy), speedw.getDegrees(), true, true);
+
+    }
+
+    
+    public void updateOdometry() {
+
+        
+
+        swervePoseEstimator.update(getOdoYaw(),
+                                   getPositions());
+
+        boolean rejectUpdate = false;
+
+        if(LimelightHelpers.getTV(Constants.Sensors.limef) && LimelightHelpers.getTV(Constants.Sensors.limeb)){
+
+            if(LimelightHelpers.getTA(Constants.Sensors.limeb) > LimelightHelpers.getTA(Constants.Sensors.limef)){
+
+                LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(Constants.Sensors.limeb);
+        
+                if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1){
+
+                    if(mt1.rawFiducials[0].ambiguity > .7){
+
+                        rejectUpdate = true;
+    
+                    }
+    
+                    if(mt1.rawFiducials[0].distToCamera > 3) {
+    
+                        rejectUpdate = true;
+    
+                    }
+    
+                }
+    
+                if(mt1.tagCount == 0){
+    
+                    rejectUpdate = true;
+    
+                }
+
+    
+                if(!rejectUpdate){
+    
+                    swervePoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.02,.02,0.005));
+                    swervePoseEstimator.addVisionMeasurement(mt1.pose,
+                                                            mt1.timestampSeconds);
+                }
+                    
+            }
+
+            else {
+
+                LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(Constants.Sensors.limef);
+      
+                if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1){
+
+                    if(mt1.rawFiducials[0].ambiguity > .7){
+
+                        rejectUpdate = true;
+  
+                   }
+  
+                    if(mt1.rawFiducials[0].distToCamera > 3) {
+                        
+                        rejectUpdate = true;
+  
+                   }
+  
+                }
+  
+                if(mt1.tagCount == 0){
+  
+                    rejectUpdate = true;
+  
+                }
+
+  
+                if(!rejectUpdate){
+  
+                    swervePoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.02,.02,0.005));
+                    swervePoseEstimator.addVisionMeasurement(mt1.pose,
+                                                         mt1.timestampSeconds);
+                }
+
+            }
+
+        }
+
+        else if(LimelightHelpers.getTV(Constants.Sensors.limeb)){
+
+            LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(Constants.Sensors.limeb);
+      
+            if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1){
+
+                if(mt1.rawFiducials[0].ambiguity > .7){
+
+                    rejectUpdate = true;
+  
+                }
+  
+                if(mt1.rawFiducials[0].distToCamera > 3) {
+  
+                    rejectUpdate = true;
+  
+                }
+  
+            }
+  
+            if(mt1.tagCount == 0){
+  
+                rejectUpdate = true;
+  
+            }
+
+  
+            if(!rejectUpdate){
+  
+                swervePoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.02,.02,0.005));
+                swervePoseEstimator.addVisionMeasurement(mt1.pose,
+                                                         mt1.timestampSeconds);
+            }
+
+        }
+
+        else if(LimelightHelpers.getTV(Constants.Sensors.limef)){
+        
+            LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(Constants.Sensors.limef);
+      
+            if(mt1.tagCount == 1 && mt1.rawFiducials.length == 1){
+
+                if(mt1.rawFiducials[0].ambiguity > .7){
+
+                    rejectUpdate = true;
+  
+                }
+  
+                if(mt1.rawFiducials[0].distToCamera > 3) {
+  
+                    rejectUpdate = true;
+  
+                }
+  
+            }
+  
+            if(mt1.tagCount == 0){
+  
+                rejectUpdate = true;
+  
+            }
+
+  
+            if(!rejectUpdate){
+  
+                swervePoseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.02,.02,0.005));
+                swervePoseEstimator.addVisionMeasurement(mt1.pose,
+                                                         mt1.timestampSeconds);
+            }
+
+        }
+        
+        swerveOdometry.resetPosition(getOdoYaw(), getPositions(), swervePoseEstimator.getEstimatedPosition());
+    }
+
+    @Override
+    public void periodic() {
+        //swerveOdometry.update(getOdoYaw(), getPositions());
+        //swervePoseEstimator.update(getOdoYaw(), getPositions());
+        // //swervePoseEstimator.addVisionMeasurement(getPose(), 0);
+        // limef = NetworkTableInstance.getDefault().getTable(Constants.Sensors.limef);
+        // limeb = NetworkTableInstance.getDefault().getTable(Constants.Sensors.limeb);        
+        
+        
+        SmartDashboard.putString("Odometry: ", swerveOdometry.getPoseMeters().getTranslation().toString());
+        SmartDashboard.putNumber("NavX", getYaw().getDegrees());
+        SmartDashboard.putNumber("NavX Odo", getOdoYaw().getDegrees());
+
+        // if(limeb.getEntry("tv").getDouble(0) == 1){
+
+        //     poseData = limeb.getEntry("botpos_wpiblue").getDoubleArray(poseData);
+        //     visionMeasurement = new Pose2d(poseData[0], poseData[1], getOdoYaw());
+        //     swervePoseEstimator.addVisionMeasurement(visionMeasurement, Timer.getFPGATimestamp());     
+
+        // }
+
+
+        updateOdometry();
+        field.setRobotPose(swervePoseEstimator.getEstimatedPosition());
+
+
+        double loggingModulesStates [] = {
+            swerveModules[0].getState().angle.getDegrees(), swerveModules[0].getState().speedMetersPerSecond * 50,
+            swerveModules[1].getState().angle.getDegrees(), swerveModules[1].getState().speedMetersPerSecond * 50,
+            swerveModules[2].getState().angle.getDegrees(), swerveModules[2].getState().speedMetersPerSecond * 50,
+            swerveModules[3].getState().angle.getDegrees(), swerveModules[3].getState().speedMetersPerSecond * 50   
+        };
+
+        SmartDashboard.putNumber("Angle FL: ", swerveModules[0].getState().angle.getDegrees());
+        SmartDashboard.putNumber("Angle FR: ", swerveModules[1].getState().angle.getDegrees());
+        SmartDashboard.putNumber("Angle RL: ", swerveModules[2].getState().angle.getDegrees());
+        SmartDashboard.putNumber("Angle RR: ", swerveModules[3].getState().angle.getDegrees());
+
+        SmartDashboard.putNumber("Angle EFL: ", swerveModules[0].getCANCoderAngle().getDegrees());
+        SmartDashboard.putNumber("Angle EFR: ", swerveModules[1].getCANCoderAngle().getDegrees());
+        SmartDashboard.putNumber("Angle ERL: ", swerveModules[2].getCANCoderAngle().getDegrees());
+        SmartDashboard.putNumber("Angle ERR: ", swerveModules[3].getCANCoderAngle().getDegrees());
+
+
+
+        SmartDashboard.putNumberArray("SwerveModuleStates", loggingModulesStates);
+
+    }
+    
+}
---
+++ Ventura_2024/src/main/java/frc/robot/subsystems/drivetrain/swerveModule.java
@@ -0,0 +1,171 @@
+package frc.robot.subsystems.drivetrain;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.StaticBrake;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+//import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.config.SwerveModuleConstants;
+import frc.lib.math.ModuleOptimizer;
+import frc.robot.Constants;
+
+public class swerveModule extends SubsystemBase {
+
+    public int moduleID;
+
+    private Rotation2d lastAngle;
+    private Rotation2d angleOffset;
+    private Rotation2d encoderOffset;
+
+    private TalonFX driveMotor;
+    private TalonFX steerMotor;
+    private CANcoder angleEncoder;
+
+    //  private final SimpleMotorFeedforward feedforward =  new SimpleMotorFeedforward(Constants.Swerve.driveKS, 
+    //                                                                                Constants.Swerve.driveKV, 
+    //                                                                                Constants.Swerve.driveKA); 
+
+    public swerveModule(int moduleID, SwerveModuleConstants moduleConstants){
+
+        this.moduleID = moduleID;
+        angleOffset = moduleConstants.angleOffset;
+        encoderOffset = moduleConstants.encoderOffset;
+
+        angleEncoder = new CANcoder(moduleConstants.cancoderID);
+        angleEncoder.getConfigurator().apply(new CANcoderConfiguration());
+        angleEncoder.getConfigurator().apply(new MagnetSensorConfigs()
+                                                .withAbsoluteSensorRange(AbsoluteSensorRangeValue.Unsigned_0To1)
+                                                .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive));
+
+    
+
+        steerMotor = new TalonFX(moduleConstants.angleMotorID);
+        steerMotor.getConfigurator().apply(new TalonFXConfiguration());
+        steerMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(Constants.Swerve.angleContinuousCurrentLimit));
+        steerMotor.setControl(new StaticBrake());
+        steerMotor.setInverted(false);
+
+        var slot0Configs1 = new Slot0Configs();
+        slot0Configs1.kP = Constants.Swerve.angleKP;
+        slot0Configs1.kI = Constants.Swerve.angleKI;
+        slot0Configs1.kD = Constants.Swerve.angleKD;
+
+        steerMotor.getConfigurator().apply(slot0Configs1);
+
+        resetToAbsolute();
+
+        driveMotor = new TalonFX(moduleConstants.driveMotorID);
+        driveMotor.getConfigurator().apply(new TalonFXConfiguration());
+        driveMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(Constants.Swerve.driveContinuousCurrentLimit));
+        driveMotor.setControl(new StaticBrake());
+        driveMotor.setInverted(false);
+
+        var slot0Configs2 = new Slot0Configs();
+        slot0Configs2.kP = Constants.Swerve.driveKP;
+        slot0Configs2.kI = Constants.Swerve.driveKI;
+        slot0Configs2.kD = Constants.Swerve.driveKD;
+
+        driveMotor.getConfigurator().apply(slot0Configs2);
+
+        lastAngle = getState().angle;
+
+    }
+
+    public void resetToAbsolute(){
+
+        double absolutePosition = getCANCoderAngle().getDegrees() - angleOffset.getDegrees();
+        steerMotor.setPosition(absolutePosition);
+
+    }
+
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop) {
+
+        desiredState = ModuleOptimizer.optimize(desiredState, getState().angle);
+    
+        setAngle(desiredState);
+        setSpeed(desiredState, isOpenLoop);
+    }
+
+    private void setAngle(SwerveModuleState desiredState) {
+
+        Rotation2d angle = (Math.abs(desiredState.speedMetersPerSecond) <= (Constants.Swerve.maxSpeed * 0.1))
+                ? lastAngle
+                : desiredState.angle;
+    
+        steerMotor.setControl(new PositionDutyCycle(angle.getDegrees()));
+        lastAngle = angle;
+
+    }
+
+    private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
+        if (isOpenLoop) {
+
+            double power = desiredState.speedMetersPerSecond / Constants.Swerve.maxSpeed;
+            driveMotor.set(power);
+        
+        } 
+        else {
+
+            driveMotor.setControl(new VelocityDutyCycle(desiredState.speedMetersPerSecond));
+              
+        }
+    }
+    public double getDrivePosition(){
+
+        return driveMotor.getPosition().getValue() * Constants.Swerve.driveConversionPositionFactor;
+
+    }
+    public double getDriveVelocity(){
+
+        return driveMotor.getVelocity().getValue() * Constants.Swerve.driveConversionVelocityFactor;
+    }
+
+
+    public Rotation2d getSteerAngle() {
+
+        double angle = steerMotor.getPosition().getValue() * Constants.Swerve.angleConversionFactor;
+        return Rotation2d.fromDegrees(angle);
+
+    }
+
+
+    public SwerveModulePosition getPosition() {
+
+        return new SwerveModulePosition(getDrivePosition(), getSteerAngle());
+
+    }
+
+    public SwerveModuleState getState() {
+
+        return new SwerveModuleState(getDriveVelocity(), getSteerAngle());
+
+    }
+
+    public SwerveModuleState getStateEncoder() {
+
+        return new SwerveModuleState(driveMotor.getVelocity().getValue(), getCANCoderAngle());
+
+    }
+
+    public Rotation2d getCANCoderAngle() {
+
+
+        double angle = ((angleEncoder.getAbsolutePosition().getValue()) * 360.0) - encoderOffset.getDegrees();
+        return Rotation2d.fromDegrees(angle);
+
+    }
+    
+}
