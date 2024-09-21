a02b933721479e277601c0636275613099f22d39
AnaLucia-Orozco <A01725774@tec.mx>
2024-09-21 14:02:47 -0600

    AutoNono

---
+++ Ventura_2024/src/main/java/frc/robot/subsystems/climber.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class climber extends SubsystemBase{
+    private TalonFX MotorRight;
+    private TalonFX MotorLeft;
+    public climber(int motor_ID){}
+
+
+public void MotorRightVelocity(double speed){}
+public void MotorRightAngle(){}
+public void MotorRightDown(){}
+public void MotorRightUp(){}
+
+    
+public void MotorLeftVelocity(double speed){}
+public void MotorLeftAngle(){}
+public void MotorLeftDown(){}
+public void MotorLeftUp(){}
+
+
+
+}
