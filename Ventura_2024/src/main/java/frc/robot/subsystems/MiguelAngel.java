d853286f62c50ef9bbcad82777b4c9571d1e4566
miguel-angel120710 <mickygzz10@gmail.com>
2024-10-09 20:12:09 -0600

    Create MiguelAngel.java

---
+++ Ventura_2024/src/main/java/frc/robot/subsystems/MiguelAngel.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class MiguelAngel extends SubsystemBase{
+
+    private TalonFX intakeMotor;
+
+    public MiguelAngel(){
+
+    }
+
+    public void take (){
+
+    }
+
+    public void spit(){
+
+    }
+
+    }
+}
