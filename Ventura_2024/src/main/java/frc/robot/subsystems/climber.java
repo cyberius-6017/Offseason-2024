0d3e12c9c51fa30a7967ec3be80ba638b00683c5
AnaLucia-Orozco <A01725774@tec.mx>
2024-10-10 18:55:16 -0600

    climberCommand

--- Ventura_2024/src/main/java/frc/robot/subsystems/climber.java
+++ Ventura_2024/src/main/java/frc/robot/subsystems/climber.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,16 +11,44 @@ public class climber extends SubsystemBase{
     public climber(int motor_ID){}
 
 
-public void MotorRightVelocity(double speed){}
-public void MotorRightAngle(){}
-public void MotorRightDown(){}
-public void MotorRightUp(){}
+public void MotorRightVelocity(double velocity){
+       MotorRight.setControl(new VelocityDutyCycle(velocity));
 
-    
-public void MotorLeftVelocity(double speed){}
-public void MotorLeftAngle(){}
-public void MotorLeftDown(){}
-public void MotorLeftUp(){}
+}
+public void MotorRightAngle(double angle){
+
+}
+
+public void MotorRightDown(double speed){
+   if (speed>0){ 
+        speed*=-1;}
+    MotorRight.set(speed);
+        
+}
+
+public void MotorRightUp(double speed){
+    if (speed<0){ 
+        speed*=-1;}
+    MotorRight.set(speed);
+}
+ 
+public void MotorLeftVelocity(double velocity){
+MotorLeft.setControl(new VelocityDutyCycle(velocity));
+}
+public void MotorLeftAngle(double angle){
+    MotorLeft.setControl(new PositionDutyCycle(angle));
+}
+public void MotorLeftDown(double speed){
+    if (speed>0){ 
+        speed*=-1;}
+    MotorLeft.set(speed);
+}
+public void MotorLeftUp(double speed){
+    if (speed<0){ 
+        speed*=-1;}
+    MotorLeft.set(speed);
+
+}
 
 
 
